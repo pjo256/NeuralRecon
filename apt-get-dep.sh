379cdb3933d2a4a7d3ef1ad4feb0bc3add10cac1
Philip Ottesen <philip.ottesen@vimeo.com>
2021-07-13 13:46:38 -0400

    fix: scripts for running on GCE

---
+++ apt-get-dep.sh
@@ -0,0 +1,3 @@
+apt-get update
+apt-get install libsparsehash-dev unzip
+apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
---
+++ gdown-dl.sh
@@ -0,0 +1,5 @@
+pip install gdown
+mkdir checkpoints && cd checkpoints
+gdown --id 1zKuWqm9weHSm98SZKld1PbEddgLOQkQV
+cd ..
+gdown https://drive.google.com/u/1/uc?id=1i7yDY-oautHgwrDPUrB7BuZytiIYhxfY&export=download
